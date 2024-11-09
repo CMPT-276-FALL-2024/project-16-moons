63b195bc2b93297d3fb25ed3e982e413dd20b534
Tyler <tiho@sfu.ca>
2024-11-09 09:31:58 -0800

    Added component background for homepage, but css isn't working

---
+++ dish-it-react/src/homepageComponents/homeBackground.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeBackground = () => {
+    return (
+    <div class="background-image">
+      <div class="filter-blur"></div>
+    </div>
+    )
+}
+
+export default HomeBackground;

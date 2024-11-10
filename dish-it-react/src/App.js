62d6aedcb61730f23c432b5a561993d6901a23c7
Tyler <tiho@sfu.ca>
2024-11-09 19:36:57 -0800

    Added chatbot react component to homepage

--- dish-it-react/src/App.js
+++ dish-it-react/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HomeBackground from './homepageComponents/homeBackground';
 import NavBar from './homepageComponents/navBar';
+import ChatBot from './homepageComponents/chatBot';
 
 function App() {
   return (
     <div>
       <HomeBackground />
       <NavBar />
+      <ChatBot />
     </div>
   );
 }

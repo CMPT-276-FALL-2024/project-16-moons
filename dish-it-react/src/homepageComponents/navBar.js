cf8220740598629e9ee747b756423669fb711609
Tyler <tiho@sfu.ca>
2024-11-09 19:31:10 -0800

    Fixed warnings for navBar and dishit logo loads for navbar

--- dish-it-react/src/homepageComponents/navBar.js
+++ dish-it-react/src/homepageComponents/navBar.js
@@ -4,19 +4,20 @@ import "../index.css";
 const NavBar = () => {
     return (
     <header className="navigation-bar">
-        <a href="#">
+        <a href="/">
             <img
-            src=".../public/images/Dish-It-Smaller.png"
+            src="/images/Dish-It-Smaller.png"
             className="image"
             alt="logo of Dish-It"
         /></a>
         <nav className="navbar">
-            <a href="#">HOME</a>
+            <a href="/">HOME</a>
             <a href="#howitworks">HOW IT WORKS</a>
             <a href="#about">ABOUT</a>
             <a
             href="https://docs.google.com/document/d/1JZCWgFncoqhTWbnXguZtalruUfB7CJaEjLOR9sbGqdo/edit?usp=sharing"
             target="_blank"
+            rel="noopener noreferrer"
             >DOCUMENTATION</a
             >
             <a href="#contactus">CONTACT US</a>

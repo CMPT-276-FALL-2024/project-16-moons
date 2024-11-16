89e98a602f0a4d63d662246d3a516ff84f85ca91
Tyler Ho <tiho@sfu.ca>
2024-11-16 15:28:27 -0800

    Changed how food fact component renders in for easier testing

--- dish-it-react/src/tests/unit/food-fact-component.test.js
+++ dish-it-react/src/tests/unit/food-fact-component.test.js
@@ -3,3 +3,15 @@ import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import foodFactComponent from '../../homepageComponents/foodFactComponent';
 
+describe("foodFactComponent", () => {
+    // First test to test if the component renders
+    test("renders foodFactComponent", () => {
+        render (<foodFactComponent />);
+        const foodFactElement = screen.getByText(/did you know/i);
+        expect(foodFactElement).toBeInTheDocument();
+    }
+    
+    // Second te
+    
+
+});

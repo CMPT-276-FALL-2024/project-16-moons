1b564244d139a81c824bd7b42ee214ae2a744a4d
Tyler Ho <tiho@sfu.ca>
2024-11-22 20:28:24 -0800

    Homewebpage functional with search recipe and will send to search recipe page with the recipe string

--- dish-it-react/src/index.js
+++ dish-it-react/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import RandomDishPage from "./randomDish/randomDishPage";
+import RecipeSpecificSearch from "./recipeSpecificSearch/recipeSpecificSearchPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,6 +12,7 @@ root.render(
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/randomdish" element={<RandomDishPage />} />
+      <Route path="/recipeSpecificSearch" element={<RecipeSpecificSearch />} />
     </Routes>
   </Router>
 );

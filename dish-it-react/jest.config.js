a247a66aa11d7bb6f6c3fed8de1eadd574be71f6
Kevintan111 <Dodobeanhi@gmail.com>
2024-11-25 18:11:04 -0800

    Update jest.config.js

--- dish-it-react/jest.config.js
+++ dish-it-react/jest.config.js
@@ -5,4 +5,7 @@ module.exports = {
     transform: {
         '^.+//.(js|jsx|tx|tsx)$': 'babel-jest',
     },
+    transformIgnorePatterns: [
+        "/node_modules/(?!your-es6-module|another-es6-module).*/"
+    ],
 };

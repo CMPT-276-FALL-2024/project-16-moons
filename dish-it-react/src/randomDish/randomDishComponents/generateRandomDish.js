42c1b65835a4c36f992ae76c986ddc9c035fa80b
Kevintan111 <Dodobeanhi@gmail.com>
2024-11-26 00:51:53 -0800

    Update generateRandomDish.js

--- dish-it-react/src/randomDish/randomDishComponents/generateRandomDish.js
+++ dish-it-react/src/randomDish/randomDishComponents/generateRandomDish.js
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ScrollUp from "../../scrollUp/scrollUp";
 
-const apiKey = process.env.REACT_APP_SPOONACULAR_API_KEY_RANDOM_DISH;
+// const apiKey = process.env.REACT_APP_SPOONACULAR_API_KEY_RANDOM_DISH;
 
 const GenerateRandomDish = () => {
   // Recipe Data
@@ -13,8 +16,17 @@ const GenerateRandomDish = () => {
     const fetchRecipeData = async () => {
       try {
         const response = await fetch(
-          "https://api.spoonacular.com/recipes/random?includeNutrition=true&number=1&apiKey=" +
-            apiKey
+          "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?includeNutrition=true&number=1",
+          {
+            method: "GET",
+            headers: {
+              "x-rapidapi-host":
+                "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+              "x-rapidapi-key":
+                "9c8d5e865bmshf815406cc9dc192p1bb800jsn4b3bab2cc079",
+              "x-rapidapi-ua": "RapidAPI-Playground",
+            },
+          }
         );
         if (!response.ok) {
           throw new Error("Failed to fetch recipe");
@@ -28,89 +40,116 @@ const GenerateRandomDish = () => {
         setLoading(false);
       }
     };
-
+    console.log("attempt");
     fetchRecipeData();
   }, []);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error} </p>;
+  const summaryFiltered = recipeData.summary.replace(/<[^>]*>/g, "").trim();
 
   return (
     recipeData && (
-      <div className="recipe">
-        <div className="hero" id="summary">
-          <div>
-            <h1>{recipeData.title}</h1>
-            <h2>Summary</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-              consectetur fugiat tempora! Recusandae veniam, distinctio libero
-              aperiam assumenda voluptate vitae quod voluptatibus provident
-              fugit mollitia voluptatem non eaque earum corporis.
-            </p>
-            {/* {recipeData.summary} */}
-            <a href="#recipeinformation">
-              <button>Let's Start Cooking! </button>
-            </a>
-          </div>
+      <div>
+        <head>
+          <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+          <title>Dish-It | Cooking All In One!</title>
+        </head>
+        <header className="header">
           <img
-            src={recipeData.image}
-            alt={recipeData.title}
-            className="heroImg"
-          />
-        </div>
-        <div className="recipeInformation" id="recipeinformation">
-          <h1>Recipe Information</h1>
-          <div className="gridContainer">
-            {/* Recipe Information Block */}
-            <div className="ingredientList gridBox">
-              <h2>Ingredients</h2>
-              <ul>
-                {recipeData.extendedIngredients.map((ingredient) => (
-                  <li key={ingredient.id || ingredient.name}>
-                    {ingredient.amount} {ingredient.unit} {ingredient.name}
+            className="logo"
+            alt="Dish-It Logo"
+            src="images/logoNavBar.png"
+          ></img>
+          <nav className="main-nav">
+            <ul className="main-nav-list">
+              <li>
+                <a className="main-nav-link" href="#home">
+                  Summary
+                </a>
+              </li>
+              <li>
+                <Link to="/" className="main-nav-link">
+                  Go back
+                </Link>
+              </li>
+              <li>
+                <a
+                  className="main-nav-link"
+                  href="https://docs.google.com/document/d/1JZCWgFncoqhTWbnXguZtalruUfB7CJaEjLOR9sbGqdo/edit?usp=sharing"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Documentation
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <section className="section-summary" id="home">
+            <h2 className="heading-secondary bottom-less-margin">
+              {recipeData.title}
+            </h2>
+            <div className="grid grid--2-cols grid--center-v">
+              {" "}
+              <div>
+                <img
+                  src={recipeData.image}
+                  alt={recipeData.title}
+                  className="summary-image"
+                />
+              </div>
+              <div>
+                <p className="summary-description summary-margin">
+                  {summaryFiltered}
+                </p>
+                <p className="summary-description">
+                  This recipe will be ready in{" "}
+                  <strong>{recipeData.readyInMinutes} minutes</strong> and is
+                  for <strong>{recipeData.servings} servings</strong>
+                </p>
+              </div>
+            </div>
+          </section>
+          <section className="section-extra-information">
+            <div className="grid grid--3-cols">
+              <div className="ingredients">
+                <h2>Ingredients</h2>
+                <ul>
+                  {recipeData.extendedIngredients.map((ingredient) => (
+                    <li key={ingredient.id || ingredient.name}>
+                      {ingredient.amount} {ingredient.unit} {ingredient.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="nutrients">
+                <h2>Nutrients</h2>
+                <ul>
+                  {" "}
+                  {recipeData.nutrition.nutrients.map((nutrient) => (
+                    <li key={nutrient.name || nutrient.id}>
+                      <strong>
+                        {nutrient.amount} {nutrient.unit}
+                      </strong>{" "}
+                      of {nutrient.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="instructions">
+                <h2>Instructions</h2>
+                {recipeData.analyzedInstructions[0].steps.map((instruction) => (
+                  <li key={instruction.number}>
+                    <strong>{instruction.number}</strong>. {instruction.step}
                   </li>
                 ))}
-              </ul>
+              </div>
             </div>
-            <div className="altInformation gridBox">
-              {/* Cooking Time & Serving Size Block */}
-              <h3>Cooking Time</h3>
-              <h3>{recipeData.readyInMinutes} minutes</h3>
-              <h3>This recipe is for..</h3>
-              <h3>{recipeData.servings} servings</h3>
-            </div>
-            <div className="nutrients gridBox">
-              <h2>Nutrients</h2>
-              <ul>
-                {recipeData.nutrition?.nutrients?.map((nutrient, index) => (
-                  <li key={nutrient.name || index}>
-                    <strong>
-                      {nutrient.amount} {nutrient.unit} {nutrient.name}
-                    </strong>
-                  </li>
-                )) || <li>No nutrient information available.</li>}
-              </ul>
-            </div>
-          </div>
-        </div>
-        {/* Instructions */}
-        <div className="instructionsBackground" id="instructions">
-          <h1>Instructions</h1>
-          <div className="instructionsGrid">
-            {recipeData.analyzedInstructions?.[0]?.steps?.length ? (
-              recipeData.analyzedInstructions[0].steps.map((instruction, index) => (
-                <div className="instructions" key={instruction.number || index}>
-                  <p>
-                    <strong>{instruction.number}</strong>. {instruction.step}
-                  </p>
-                </div>
-              ))
-            ) : (
-              <p>No instructions available.</p>
-            )}
-          </div>
-        </div>
+          </section>
+        </main>
+        <ScrollUp></ScrollUp>
       </div>
     )
   );

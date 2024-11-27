6ff38b7dc1eb42d39483c40e93c8411ee4ec3f89
Gabriel Bello <johngabrielbello@outlook.com>
2024-11-27 14:56:01 -0800

    Fixing merge

--- dish-it-react/src/recipeSpecificSearch/recipeSpecificSearchComponents/recipeSpecificSearchBody.js
+++ dish-it-react/src/recipeSpecificSearch/recipeSpecificSearchComponents/recipeSpecificSearchBody.js
@@ -40,6 +40,12 @@ export default function RecipeSpecificSearchBody() {
         document.getElementById("ingredient-list-input").value = ""
     }
 
+    const enterKeyDown = (event) => {
+        if (event.key === "Enter") {
+            setRecipeToFetch(event.target.value);
+        }
+    };
+
     return (
         <body>
             <section>
@@ -50,29 +56,39 @@ export default function RecipeSpecificSearchBody() {
                             id="ingredient-list-input"
                             type="text"
                             placeholder="Search recipes here!"
+                            onKeyUp={enterKeyDown}
                         ></input>
                         <button className="search-button" onClick={setNewRecipe}>
                             <img id="search-icon" src="/images/search-icon.png" alt="search icon"/>
                         </button> 
-                        <h1> Searching for {recipeToFetch}</h1>
+                        {/* If a proper search is inputted and results are returned */}
+                        {data && data.hits.length > 0&& (
+                            <h1> Searching for {recipeToFetch}</h1>
+                        )}
+                        {/* If no results are returned */}
+                        {data && data.hits.length === 0 && (
+                            <h1> No recipes found for {recipeToFetch}</h1>
+                        )}
                     </div>
+                    {/* If there are recipes to be displayed */}
                     {data && data.hits.length > 0 && (
                         <div className="recipe-cards-container">
+                            {/* Display one row of cards (Adjust the 0, 10 in slice for which indexes to display) */}
                             <div className="vertical-flex-container">
                                 {data.hits.slice(0, 10).map((hit, index) => (
                                     <RecipeCard
                                         key={index}
-                                        recipeImage={hit.recipe.image}
-                                        recipeName={hit.recipe.label}
+                                        data={hit}
+                                        searchInput={recipeToFetch}
                                     />
                                 ))}
                             </div>
+                            {/* Another row of indexes 11-20 of hits */}
                             <div className="vertical-flex-container">
                                 {data.hits.slice(11, 20).map((hit, index) => (
                                     <RecipeCard
                                         key={index}
-                                        recipeImage={hit.recipe.image}
-                                        recipeName={hit.recipe.label}
+                                        data={hit}
                                     />
                                 ))}
                             </div>

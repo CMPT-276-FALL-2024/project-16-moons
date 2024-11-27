dd3ff40609f64c15e4f136b1937fd754efe82511
Tyler Ho <tiho@sfu.ca>
2024-11-27 00:05:40 -0800

    Added all aspects of the recipe that could be fetched from API

--- dish-it-react/src/recipeOverview/recipeOverviewComponents/recipeOverviewBody.js
+++ dish-it-react/src/recipeOverview/recipeOverviewComponents/recipeOverviewBody.js
@@ -7,7 +7,7 @@ function RecipeOverviewBody() {
     const recipe = recipeData.recipe;
     console.log(recipeData);
     return (
-        <div>
+        <body>
             <h1>Recipe Overview: {recipe.label}</h1>
             <img src={recipe.image} alt={recipe.label} />
             <ul className="main-details">
@@ -20,11 +20,43 @@ function RecipeOverviewBody() {
 
             <h2>Ingredients</h2>
             <ul>
-                {recipe.ingredientLines.map((ingredient) => (
-                    <li>{ingredient}</li>
+                {recipe.ingredientLines.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
                 ))}
             </ul>
-        </div>
+
+            <h2>Health Labels</h2>
+            <ul>
+                {recipe.healthLabels.map((label, index) => (
+                    <li key={index}>{label}</li>
+                ))}
+            </ul>
+
+            <h2>Caution Labels</h2>
+            <ul>
+                {recipe.cautions.map((label, index) => (
+                    <li key={index}>{label}</li>
+                ))}
+            </ul>
+            
+            <h2>Nutrient List</h2>
+            <ul>
+                <li>{round(recipe.totalNutrients.ENERC_KCAL.quantity)} {recipe.totalNutrients.ENERC_KCAL.unit} of Calories</li>
+                <li>{round(recipe.totalNutrients.FAT.quantity)} {recipe.totalNutrients.FAT.unit} of Fat</li>
+                {recipe.digest[0].sub.map((nutrition, index) => (
+                    <li key={index}>{round(nutrition.total)} {nutrition.unit} of {nutrition.label}</li>
+                ))}
+                {recipe.digest[1].sub.map((nutrition, index) => (
+                    <li key={index}>{round(nutrition.total)} {nutrition.unit} of {nutrition.label}</li>
+                ))}
+                {recipe.digest.map((nutrition, index) => (
+                    <li key={index}>{round(nutrition.total)} {nutrition.unit} of {nutrition.label}</li>
+                ))}
+            </ul>
+
+            <a href={recipe.url} target="_blank" rel="noopener noreferrer">Click here to see the full recipe</a>
+            
+        </body>
     );
     }
 

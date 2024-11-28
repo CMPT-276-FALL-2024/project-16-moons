804baf29a4e1e2f9b1dab62cc54f624043df6ff9
Gabriel Bello <johngabrielbello@outlook.com>
2024-11-28 01:17:17 -0800

    Got rid of documenation in the nav bar

--- dish-it-react/src/recipeSpecificSearch/recipeSpecificSearchComponents/recipeSpecificSearchNavbar.js
+++ dish-it-react/src/recipeSpecificSearch/recipeSpecificSearchComponents/recipeSpecificSearchNavbar.js
@@ -3,29 +3,24 @@ import { Link } from "react-router-dom";
 function RecipeSpecificSearchNavbar() {
   return (
     <header className="header">
-        {/* Link to the home page */}
-        <Link to="/">
+      {/* Link to the home page */}
+      <Link to="/">
         <img
-            src="/images/logoNavBar.png"
-            className="logo"
-            alt="logo
+          src="/images/logoNavBar.png"
+          className="logo"
+          alt="logo
             of Dish-It"
-        /></Link>
-        <nav className="main-nav-list">
-            {/* Spelled out link to home page */}
-            <Link className="main-nav-link" to="/">BACK TO HOME</Link>
-            {/* Link to documentation */}
-            <a
-              href="https://docs.google.com/document/d/1JZCWgFncoqhTWbnXguZtalruUfB7CJaEjLOR9sbGqdo/edit?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="main-nav-link"
-            >
-              DOCUMENTATION
-            </a>
-        </nav>
+        />
+      </Link>
+      <nav className="main-nav-list">
+        {/* Spelled out link to home page */}
+        <Link className="main-nav-link" to="/">
+          BACK TO HOME
+        </Link>
+        {/* Link to documentation */}
+      </nav>
     </header>
-  )
+  );
 }
 
 export default RecipeSpecificSearchNavbar;

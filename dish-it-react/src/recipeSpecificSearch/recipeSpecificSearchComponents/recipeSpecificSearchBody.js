87225fe52d9bfe354af3a10ecc2f05b4be9e4ba1
Tyler Ho <tiho@sfu.ca>
2024-11-29 13:00:33 -0800

    Started adding styling to page. Saving progress of search bar and search button

--- dish-it-react/src/recipeSpecificSearch/recipeSpecificSearchComponents/recipeSpecificSearchBody.js
+++ dish-it-react/src/recipeSpecificSearch/recipeSpecificSearchComponents/recipeSpecificSearchBody.js
@@ -47,54 +47,57 @@ export default function RecipeSpecificSearchBody() {
     };
 
     return (
-        <body>
+        <body className="recipe-specific-search-body">
             <section>
-                <div className="horizontal-flex-container">
-                    {/* Add More Ingredients & Current Ingredients  */}
-                    <div className="vertical-flex-container">
-                        <input
-                            id="ingredient-list-input"
-                            type="text"
-                            placeholder="Search recipes here!"
-                            onKeyUp={enterKeyDown}
-                        ></input>
-                        <button className="search-button" onClick={setNewRecipe}>
-                            <img id="search-icon" src="/images/search-icon.png" alt="search icon"/>
-                        </button> 
-                        {/* If a proper search is inputted and results are returned */}
-                        {data && data.hits.length > 0&& (
+                {/* Search Bar and Seach Button  */}
+                <div className="search-container">
+                    <input
+                        id="ingredient-list-input"
+                        type="text"
+                        placeholder="Search recipes here!"
+                        onKeyUp={enterKeyDown}
+                    ></input>
+                    <button className="search-button" onClick={setNewRecipe}>
+                        <img id="search-icon" src="/images/search-icon.png" alt="search icon"/>
+                    </button> 
+                </div>
+                <div className="searching-name-container">
+                    {/* If a proper search is inputted and results are returned */}
+                    {data && data.hits.length > 0 && (
                             <h1> Searching for {recipeToFetch}</h1>
                         )}
                         {/* If no results are returned */}
                         {data && data.hits.length === 0 && (
                             <h1> No recipes found for {recipeToFetch}</h1>
                         )}
-                    </div>
-                    {/* If there are recipes to be displayed */}
-                    {data && data.hits.length > 0 && (
-                        <div className="recipe-cards-container">
-                            {/* Display one row of cards (Adjust the 0, 10 in slice for which indexes to display) */}
-                            <div className="vertical-flex-container">
-                                {data.hits.slice(0, 10).map((hit, index) => (
-                                    <RecipeCard
-                                        key={index}
-                                        data={hit}
-                                        searchInput={recipeToFetch}
-                                    />
-                                ))}
-                            </div>
-                            {/* Another row of indexes 11-20 of hits */}
-                            <div className="vertical-flex-container">
-                                {data.hits.slice(11, 20).map((hit, index) => (
-                                    <RecipeCard
-                                        key={index}
-                                        data={hit}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-                    )}
                 </div>
+
+                
+                {/* If there are recipes to be displayed */}
+                {data && data.hits.length > 0 && (
+                    <div className="recipe-cards-container">
+                        {/* Display one row of cards (Adjust the 0, 10 in slice for which indexes to display) */}
+                        <div className="vertical-flex-container">
+                            {data.hits.slice(0, 10).map((hit, index) => (
+                                <RecipeCard
+                                    key={index}
+                                    data={hit}
+                                    searchInput={recipeToFetch}
+                                />
+                            ))}
+                        </div>
+                        {/* Another row of indexes 11-20 of hits */}
+                        <div className="vertical-flex-container">
+                            {data.hits.slice(11, 20).map((hit, index) => (
+                                <RecipeCard
+                                    key={index}
+                                    data={hit}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                )}
+
             </section>
         </body>
     )

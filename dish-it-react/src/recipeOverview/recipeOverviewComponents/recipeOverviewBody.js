d8627c49f6041ccb85d477d3cdfe0feb61f7b93c
Gabriel Bello <johngabrielbello@outlook.com>
2024-12-01 14:11:58 -0800

    Added button shadowing and added documentation to most pages that were missing it.  :D

--- dish-it-react/src/recipeOverview/recipeOverviewComponents/recipeOverviewBody.js
+++ dish-it-react/src/recipeOverview/recipeOverviewComponents/recipeOverviewBody.js
@@ -11,7 +11,7 @@ function RecipeOverviewBody() {
       <main>
         <section className="hero" id="main">
           <h1 className="recipe-title">{recipe.label}</h1>
-          <div className="grid grid--2-cols">
+          <div className="grid grid--3-cols">
             <img
               src={recipe.image}
               className="recipe-image"
@@ -27,85 +27,42 @@ function RecipeOverviewBody() {
                 </ul>
               </div>
               <p className="servings-text">
-                This Dish is good for {recipe.yield} servings
+                <strong>This recipe is good for {recipe.yield} servings</strong>
               </p>
               <a
                 href={recipe.url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="recipe-link"
+                className="recipe-link btn"
               >
-                <span class="recipe-link-name">View Full Recipe</span>
+                <span class="recipe-link-name">
+                  <strong>View Full Recipe</strong>
+                </span>
               </a>
-            </div>
-          </div>
-          <div class="nutrition-facts">
-            <h1>Nutrition Facts</h1>
-            <div>
-              <p>Calories: {round(recipe.calories)}</p>
-              {recipe.digest.map((nutrition, index) => (
-                <p key={index}>
-                  {round(nutrition.total)} {nutrition.unit} of {nutrition.label}{" "}
-                  ({round(nutrition.daily, 1)}% of daily needs)
+              {recipe.cautions && recipe.cautions.length > 0 && (
+                <p className="caution-text">
+                  <strong>CAUTION</strong> this dish includes:{" "}
+                  {recipe.cautions.join(", ")}
                 </p>
-              ))}
+              )}
+            </div>
+            <div class="nutrition-facts">
+              <h1>Nutrition Facts</h1>
+              <div>
+                <p>
+                  {round(recipe.calories)} of <strong>Calories</strong>
+                </p>
+                {recipe.digest.map((nutrition, index) => (
+                  <p key={index}>
+                    {round(nutrition.total)} {nutrition.unit} of{" "}
+                    <strong>{nutrition.label}</strong>
+                  </p>
+                ))}
+              </div>
             </div>
           </div>
         </section>
       </main>
-      <ul className="main-details">
-        <li></li>
-        <li>Servings: {recipe.yield}</li>
-        {recipe.totalTime !== 0 && <li>Time: {recipe.totalTime} minutes</li>}
-      </ul>
-
-      {/* All fetched ingredients for recipe from API*/}
-      <h2>Ingredients</h2>
-      <ul>
-        {recipe.ingredientLines.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
-
-      {/* All health labels associated with recipe */}
-      <h2>Health Labels</h2>
-      <ul>
-        {recipe.healthLabels.map((label, index) => (
-          <li key={index}>{label}</li>
-        ))}
-      </ul>
-
-      {/* All caution labels associated with recipe */}
-      <h2>Caution Labels</h2>
-      <ul>
-        {recipe.cautions.map((label, index) => (
-          <li key={index}>{label}</li>
-        ))}
-      </ul>
-
-      {/* List of nutrients */}
-      <h2>Nutrient List</h2>
-      <ul>
-        <li>
-          {round(recipe.totalNutrients.ENERC_KCAL.quantity)}{" "}
-          {recipe.totalNutrients.ENERC_KCAL.unit} of Calories
-        </li>
-
-        <h2>Fats</h2>
-        {recipe.digest[0].sub.map((nutrition, index) => (
-          <li key={index}>
-            {round(nutrition.total)} {nutrition.unit} of {nutrition.label} (
-            {round(nutrition.daily, 1)}% of daily needs)
-          </li>
-        ))}
-
-        <h2>Carbs</h2>
-      </ul>
-
-      {/* Link to full recipe */}
-      <a href={recipe.url} target="_blank" rel="noopener noreferrer">
-        Click here to see the full recipe
-      </a>
     </div>
   );
 }

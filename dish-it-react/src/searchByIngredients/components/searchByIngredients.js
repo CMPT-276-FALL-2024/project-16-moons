da21e4c1921838519be98d6619aac46d6b14b163
Kevintan111 <Dodobeanhi@gmail.com>
2024-12-03 23:36:32 -0800

    Implemented separate URL for search results for easier back and forth navigation

--- dish-it-react/src/searchByIngredients/components/searchByIngredients.js
+++ dish-it-react/src/searchByIngredients/components/searchByIngredients.js
@@ -1,85 +1,28 @@
 import React, { useState } from "react";
 import IngredientInput from "./ingredientsInput";
 import IngredientList from "./ingredientList";
-import RecipeList from "./recipeList";
 import SearchButton from "./searchButton";
 
-const apiKey = process.env.REACT_APP_X_RAPIDAPI_KEY;
-const apiUa = process.env.REACT_APP_X_RAPIDAPI_UA;
-const apiHost = process.env.REACT_APP_X_RAPID_HOST;
-
 const SearchByIngredients = () => {
-
     const [ingredients, setIngredients] = useState([]);
-    const [recipes, setRecipes] = useState([]);
-    const [error, setError] = useState(null);
 
-    // add into list if not blank/already existed
     const addIngredient = (ingredient) => {
         if (ingredient && !ingredients.includes(ingredient)) {
-            setIngredients((addedIngredients) => [...addedIngredients, ingredient]);
+            setIngredients([...ingredients, ingredient]);
         }
     };
 
     const removeIngredient = (ingredientToRemove) => {
-        setIngredients((addedIngredients) =>
-        addedIngredients.filter((ingredient) => ingredient !== ingredientToRemove)
-        );
+        setIngredients(ingredients.filter((ingredient) => ingredient !== ingredientToRemove));
     };
-
-    //* Main logic: work flow is going to look like this:
-    //* 1: search for a bunch of recipes using ingredients
-    //* 2: when user press on one, search for the full detail using that specific ID
-    //* 3: doesn't have instructions, go to external website to get all.
-    // When Search is cliced, fetch from API
-    const handleSearchClick = async () => {
-        if (ingredients.length === 0) {
-            setError("Please add some ingredients");
-            return;
-        }
-
-        try {
-            const query = ingredients.join("%2C");   
-            // default: 21 recipes, ignore pantry ingredients, minimize missing ingredients
-            const response = await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=${query}&number=21&ignorePantry=true&ranking=2`,
-                {
-                    method: "GET",
-                    headers: {
-                        "x-rapidapi-host": apiHost,
-                        "x-rapidapi-key": apiKey,
-                        "x-rapidapi-ua": apiUa,
-                    },
-                }
-            )
-
-            if (!response.ok) {
-                throw new Error("Failed to fetch recipes.");
-            }
-
-            const data = await response.json();
-            setRecipes(data);
-            } catch (err) {
-            setError(err.message);
-            }
-};
 
     return (
         <div className="page">
             <div className="sidebar">
-                {/* Most field name are in their own files */}
-                <div>
-                    <IngredientInput addIngredient={addIngredient} />
-                </div>
-
-                <div>
-                    <IngredientList
-                        ingredients={ingredients}
-                        removeIngredient={removeIngredient}
-                    />
-                </div>
-                <SearchButton onSearchClick={handleSearchClick} />
+                <IngredientInput addIngredient={addIngredient} />
+                <IngredientList ingredients={ingredients} removeIngredient={removeIngredient} />
+                <SearchButton ingredients={ingredients} />
             </div>
-            <RecipeList recipes={recipes} />
         </div>
     );
 };
--- dish-it-react/src/searchByIngredients/overview.js
+++ dish-it-react/src/searchByIngredients/overview.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "../css/general.css";
+import "../css/recipeOverview.css"
+import "../css/randomDishPage.css"
+import SearchByIngredientsNavbar from "./components/searchByIngredientsNavbar.js";
 
 const Overview = () => {
     const { id } = useParams(); // Extract recipe ID from URL
@@ -43,21 +47,80 @@ const Overview = () => {
         return <p>Loading...</p>; // Display loading while fetching data
     }
 
+    const protein = recipeDetails.summary.match(/(\d+g of protein)/)?.[1];
+    const fat = recipeDetails.summary.match(/(\d+g of fat)/)?.[1];
+    const calories = recipeDetails.summary.match(/(\d+ calories)/)?.[1];
+
     return (
-        <div className="recipe-overview">
-            <h1>{recipeDetails.title}</h1>
+        <div>
+            <SearchByIngredientsNavbar></SearchByIngredientsNavbar>
+            <main>
+                <section className = 'hero' id = 'main'>
+                    <div className="grid grid--3-cols">
+                        <h1 className = 'recipe-title'>{recipeDetails.title}</h1>
 
-            <img src={recipeDetails.image} alt={recipeDetails.title} />
+                        <img
+                        src={recipeDetails.image}
+                        className="recipe-image"
+                        alt={recipeDetails.title}
+                        />
 
-            {/*Using dangerous code to render HTML inside response */}
-            <p>
-                <strong>Summary:</strong>
-                <p dangerouslySetInnerHTML={{ __html: recipeDetails.summary }} />
-            </p>
-            
-            <a href={recipeDetails.sourceUrl} target="_blank" rel="noopener noreferrer">
-                View Full Recipe
-            </a>
+                        <div className="main-details">
+
+                            <div className="section-ingredients">
+                                <h2>Ingredients</h2>
+                                <ul className="ingredients-list">
+                                    {recipeDetails.extendedIngredients.map((ingredient,index)=> (
+                                        <li key={ingredient.id}>
+                                            <b>{ingredient.amount} {ingredient.unit}</b> of {ingredient.nameClean}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+
+                            <div className="section-ingredients">
+                                <h2>Nutrition Facts</h2>
+                                <li>
+                                       <b>
+                                        <p>{calories}</p>
+                                        <p>{protein}</p>
+                                        <p>{fat}</p>
+                                        </b> 
+                                </li>
+                            </div>
+
+                            <div className="instructions">
+                                <h2>How to Cook:</h2>
+                                {recipeDetails.analyzedInstructions.map((instruction, index) => (
+                                    <div key={index}>
+                                        <ol>
+                                            {instruction.steps.map((step) => (
+                                                <li key={step.number}>
+                                                    <b>Step {step.number}:</b> {step.step}
+                                                </li>
+                                            ))}
+                                        </ol>
+                                    </div>
+                                ))}
+                            </div>
+
+                            <p className="servings-text">
+                                <strong>This recipe is good for <b>{recipeDetails.servings}</b> servings</strong>
+                            </p>
+                            <a
+                                href={recipeDetails.sourceUrl}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="recipe-link btn"
+                            >
+                                <span class="recipe-link-name">
+                                <strong>View Full Recipe</strong>
+                                </span>
+                            </a>
+                        </div>
+                    </div>                    
+                </section>
+            </main>
         </div>
     );
 };

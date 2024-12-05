8c861b27598ca85b50d5748247b34c178af7d482
Tyler Ho <tiho@sfu.ca>
2024-12-05 15:19:40 -0800

    Only allowed correct formatting

--- dish-it-react/src/searchByIngredients/components/ingredientsInput.js
+++ dish-it-react/src/searchByIngredients/components/ingredientsInput.js
@@ -5,10 +5,30 @@ import React, {useState} from "react";
 const IngredientInput = ({addIngredient}) => {
     const [input, setInput] = useState("");
 
+    // Helper function to check if the input is valid
+    const checkInput = (input) => {
+        // The input can only contain letters
+        const regex = /^[a-zA-Z\s]*$/;
+        if (input === "") {
+            // If the input is empty then an alert will be shown
+            alert("Recipe Search Bar must not be empty!");
+            return false;
+        }
+        if (!regex.test(input)) {
+            // If the input contains anything other than letters then an alert will be shown
+            alert("Recipe Search Bar can only contain letters!");
+            return false;
+        }
+        return true;
+    };
+
     const handleAdd = () => {
-        if (input.trim()) {
-            addIngredient(input.trim());
-            setInput("");
+        const validInput = checkInput(document.getElementById("ingredient-list-input-SBI").value);
+        if (validInput) {
+            if (input.trim()) {
+                addIngredient(input.trim());
+                setInput("");
+            }
         }
     };
 
@@ -26,6 +46,7 @@ const IngredientInput = ({addIngredient}) => {
             <div className="ingredient-input">
                 <input
                 type="text"
+                id="ingredient-list-input-SBI"
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
                 onKeyDown={pressedEnter}
